d0701e0ca2236be04e462b9d28667c52dc512f67
bahaaCs <bahaaaboukhalil@gmail.com>
2024-11-23 18:13:28 +0200

    adding a route for getting news

--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -4,7 +4,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\News;
+
 class NewsController extends Controller
 {
-    
+    function get_news() {
+        $news = News::all();
+
+        return response()->json([
+            "news" => $news
+        ]);
+    }   
 }
--- routes/api.php
+++ routes/api.php
@@ -4,5 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TestController;
 use App\Http\Controllers\UsersController;
+use App\Http\Controllers\NewsController;
 
 Route::get('/user', [UsersController::class, 'get_users']);
+Route::get('/news', [NewsController::class, 'get_news']);
