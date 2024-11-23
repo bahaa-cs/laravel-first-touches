26866d0cef50706279c2fc0426ef7164e00af040
bahaaCs <bahaaaboukhalil@gmail.com>
2024-11-23 18:09:22 +0200

    creating controller + model for news

---
+++ app/Models/News.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class News extends Model
+{
+    protected $fillable = [
+        'content',
+        'from_age',
+        'to_age'
+    ];
+}
