28ed845c58db648f63fd104a630ab3f282a9b648
bahaaCs <bahaaaboukhalil@gmail.com>
2024-11-23 16:41:39 +0200

    migrating foreign key in users to types

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->unsignedBigInteger('types_id');
+            $table->foreign('types_id')->references('id')->on('types');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
