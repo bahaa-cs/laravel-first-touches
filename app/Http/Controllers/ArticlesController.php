3c312da28841cb4e03ad267239bd0cc1fc0370f2
bahaaCs <bahaaaboukhalil@gmail.com>
2024-11-23 21:41:09 +0200

    validating create_article error

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -7,16 +7,23 @@ use App\Models\Article;
 class ArticlesController extends Controller
 {
     function create_article(Request $request) {
+        try{
 
-        $article = Article::create([
-            "title" => $request->title,
-            "users_id" => $request->users_id,
-            "news_id" => $request->news_id
-        ]);
-
-        return response()->json([
-            "new_article" => $article
-        ]);
+            $article = Article::create([
+                "title" => $request->title,
+                "users_id" => $request->users_id,
+                "news_id" => $request->news_id
+            ]);
+    
+            return response()->json([
+                "new_article" => $article
+            ]);
+        }
+        catch(\Exception $e){
+            return response()->json([
+                "message" => "no such user or news id."
+            ]);    
+        }
    
 }
 }
